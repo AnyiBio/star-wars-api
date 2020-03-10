732704a6ad5420aed2c0680b5e59cfe4c69946db
Anyi Jaramillo <anyi.jar@gmail.com>
2020-03-10 17:07:33 +0100

    star wars api

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 "use strict";
 
-const axios = require("axios");
-const url = env.process.API_STARWARS;
+// const axios = require("axios");
+//
+require("dotenv").config();
+const webServer = require("./app/webserver");
+
+const port = process.env.PORT;
+const url = process.env.API_STARWARS;
+
+async function initApp() {
+  try {
+    await webServer.listen(port);
+    console.log(`Server listening on port ${port}`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+initApp();
